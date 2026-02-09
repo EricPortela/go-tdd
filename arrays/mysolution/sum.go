d074ce638171f64c9461e10d33a8ca4019d080a4
Eric Portela <ericq.portela@gmail.com>
2026-02-09 10:48:51 +0100

    feat: add solutions for arrays, for and integers

---
+++ arrays/mysolution/sum.go
@@ -0,0 +1,20 @@
+package mysolution
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(sliceOfNumbers ...[]int) []int {
+
+	sum := make([]int, len(sliceOfNumbers))
+
+	for _, numbers := range sliceOfNumbers {
+		sum = append(sum, Sum(numbers))
+	}
+
+	return sum
+}
